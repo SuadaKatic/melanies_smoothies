c2222510b0bd27d4fd2339fe93a1349ff4cdf3fe
SuadaKatic <87992073+SuadaKatic@users.noreply.github.com>
2026-01-21 18:55:47 -0500

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -8,6 +8,7 @@ st.title(f"Customize Your Smoothie!:cup_with_straw:")
 st.write(
   """Choose the fruits you want in your smoothie!
   """)
+import streamlit as st 
 
 name_on_order = st.text_input('Name on Smoothie:')
 st.write('The name on your Smoothie will be:', name_on_order)
